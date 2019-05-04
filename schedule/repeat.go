e524ed423bb6f9eb80ddb0c30e096c59b36284e3
Rangding Zhang <love-nankai@163.com>
2019-05-04 16:03:18 +0800

    Update repeat.go

--- schedule/repeat.go
+++ schedule/repeat.go
@@ -2,18 +2,16 @@ package schedule
 
 import "time"
 
-//function: Non-blocking loop timer, do action per delay time
-//return: stop control
 func Repeat(action func(), delay time.Duration) chan bool {
 	stop := make(chan bool)
 	go func() {
 		for {
-			action()
 			select {
 			case <-time.After(delay):
 			case <-stop:
 				return
 			}
+			action()
 		}
 	}()
 	return stop
