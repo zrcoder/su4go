a7d3db0c5e749c79951f94248f60959b4ede3a61
Rangding Zhang <love-nankai@163.com>
2020-06-24 11:50:06 +0800

    Update host_test.go

--- sys_info/host_test.go
+++ sys_info/host_test.go
@@ -3,5 +3,7 @@ package sys_info
 import "testing"
 
 func TestExternalIP(t *testing.T) {
-	t.Log(ExternalIP())
+	t.Log(ExternalIPOfType(IpV4))
+	t.Log(ExternalIPOfType(IpV6))
+	t.Log(ExternalIPOfType(3))
 }
